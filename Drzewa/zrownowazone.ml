dd2a8f11d23c5649100cad5c033e67be4197d0ba
xgwozdzcfs <93796898+xgwozdzcfs@users.noreply.github.com>
2021-11-11 18:16:08 +0100

    Add files via upload

--- Drzewa/zrownowazone.ml
+++ Drzewa/zrownowazone.ml
@@ -1,1 +1,24 @@
 open List;;
+type 'a tree =  Node of 'a * 'a tree list;;
+let rec fold_tree f (Node (x, l)) = 
+  f x (map (fold_tree f) l);;
+
+exception Nie;;
+
+let zrownowazone drzewo =
+  let porownaj l =
+    match l with
+    | [] -> -1 
+    | [x] -> x 
+    | h::t -> fold_left (fun a e -> if e = a then a else raise Nie) h t 
+  in 
+  try let _ = fold_tree (fun _ l -> porownaj l - 1) drzewo in true with
+    Nie -> false;;
+
+
+let drzewko = Node(1, [(Node(2, [])); Node(3, [Node(4, [])]); Node(5, [Node(6, [Node(7,[])])])]);; 
+zrownowazone drzewko;; 
+let drzewko2 = Node(1, []);;
+zrownowazone drzewko2;;
+let drzewko3 = Node(1, [Node (2, [Node(3, []); Node(4, [])]); Node (5, [Node(6, []); Node(7, [])])]);;
+zrownowazone drzewko3;;

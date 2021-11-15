e67de7b44529b292e27a77a957a6972e18b89513
xgwozdzcfs <93796898+xgwozdzcfs@users.noreply.github.com>
2021-11-16 00:16:20 +0100

    Add files via upload

---
+++ 1.Kolokwium/levels.ml
@@ -0,0 +1,16 @@
+open List;; 
+type 'a tree = Node of 'a tree * 'a * 'a tree | Leaf;; 
+let rec fold_tree f a t =
+  match t with
+  | Leaf -> a
+  | Node(l, x, r) -> f x (fold_tree f a l) (fold_tree f a r);;
+
+let levels t = 
+  let f x l r = function
+    | [] -> [x]::l (r []) 
+    | h::t -> (x::h)::l (r t) in
+  fold_tree f (fun x -> x) t [];;
+        
+let drzewko = Nodek(1, [Nodek(2, []); Nodek(3, [Nodek(4, [])]); Nodek(4, [])]);;
+let drzewko1 = Node(Leaf, 3, Node(Node(Leaf, 5, Leaf), 4, Node(Leaf, 6, Leaf)));;
+levels drzewko1;;
